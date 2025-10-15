6a3d3b3a7a823c0d9818381d36c2f762ebc1b2d6
mabushanab <m.m.abushanab94@gmail.com>
2025-10-15 14:20:16 +0300

    ...

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,49 +2,6 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'screens/login_page.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: LoginPage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  void getnext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var fav = <WordPair>[];
-  void togglefavs() {
-    if (fav.contains(current)) {
-      fav.remove(current);
-    } else {
-      fav.add(current);
-    }
-    print(fav);
-    notifyListeners();
-  }
-}
-
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -106,5 +63,24 @@ class BigCard extends StatelessWidget {
         child: Text(pair.asLowerCase, style: style),
       ),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  void getnext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var fav = <WordPair>[];
+  void togglefavs() {
+    if (fav.contains(current)) {
+      fav.remove(current);
+    } else {
+      fav.add(current);
+    }
+    print(fav);
+    notifyListeners();
   }
 }
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthService {
+  final String baseUrl = 'http://localhost:9090'; // for Android emulator
+  final storage = const FlutterSecureStorage();
+
+  Future<bool> register(String username, String password) async {
+    final url = Uri.parse('$baseUrl/auth/register');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'username': username, 'password': password}),
+    );
+    return response.statusCode == 200;
+  }
+
+  Future<bool> login(String username, String password) async {
+    final url = Uri.parse('$baseUrl/auth/login');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'username': username, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      await storage.write(key: 'token', value: data['token']);
+      return true;
+    }
+    return false;
+  }
+
+  Future<String?> getToken() async {
+    return await storage.read(key: 'token');
+  }
+
+  Future<void> logout() async {
+    await storage.delete(key: 'token');
+  }
+}
