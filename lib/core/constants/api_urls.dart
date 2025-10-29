e1ded81a6e83ed0c0ccc7613d9195f6088c255b0
mabushanab <m.m.abushanab94@gmail.com>
2025-10-29 09:48:52 +0300

    ...

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -2,6 +2,6 @@
 
 // const String baseUrl = 'http://localhost:9090';
 // const String baseUrl = 'http://192.168.100.8:9090';
-const String baseUrl = 'http://178.128.162.165:9090/api';
+const String baseUrl = 'http://178.128.162.165/api';
 
 
